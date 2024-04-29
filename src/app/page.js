66b3b219102aa6a43037835655193aec2a62c9a9
tejon17584 <alejandro.tejada.ui@gmail.com>
2024-04-28 23:39:57 -0600

    adding utils

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Script from "next/script";
 import Header from "./components/Header";
 import Select from "./components/Select";
 import Input from './components/Input';
+import { getDevices } from "./utils/mediaDevices";
 
 
 export default function Home() {
@@ -15,12 +16,26 @@ export default function Home() {
 
   const [participantToken, setParticipantToken] = useState(""); // which is necessary for participants to join a stage.
 
-  /**
-    * Initialize after the client is loaded
-    */
   const initialize = async () => {
+    // Call the handleDeviceUpdate function to update the video and audio devices
+    handleDeviceUpdate();
     // Set the value of isInitializeComplete to true
     setIsInitializeComplete(true);
+  };
+
+  const handleDeviceUpdate = async () => {
+    try {
+      const { videoDevices, audioDevices } = await getDevices();
+      setVideoDevices(videoDevices);
+      setSelectedVideoDeviceId(videoDevices[0]?.deviceId);
+
+      setAudioDevices(audioDevices);
+      setSelectedAudioDeviceId(audioDevices[0]?.deviceId);
+    } catch (error) {
+      // Handle any errors that may occur during the device update process
+      console.error("An error occurred during device update:", error);
+      // You can add additional error-handling logic here as needed
+    }
   };
 
 
